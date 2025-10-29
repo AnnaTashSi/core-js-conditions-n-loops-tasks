bf4ec593266d7a49a583d7dc0741d3eda5e004a6
AnnaTashSi <si9110219358@gmail.com>
2025-10-29 13:01:11 +0300

    add solution

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -544,63 +544,8 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(str, iterations) {
-  const n = str.length;
-  if (n === 0) return str;
-
-  const perm = [];
-  for (let i = 0; i < n; i += 1) {
-    if (i % 2 === 0) {
-      perm[i] = i / 2;
-    } else {
-      perm[i] = Math.floor(n / 2) + Math.floor(i / 2);
-    }
-  }
-
-  function applyPerm(arr, perm) {
-    const res = [];
-    for (let i = 0; i < n; i += 1) {
-      res[i] = arr[perm[i]];
-    }
-    return res;
-  }
-
-  function permPower(perm, power) {
-    let result = [];
-    for (let i = 0; i < n; i += 1) {
-      result[i] = i;
-    }
-    let base = perm.slice();
-
-    while (power > 0) {
-      if (power % 2 === 1) {
-        result = applyPerm(result, base);
-      }
-      let powerty = power;
-      powerty = Math.floor(powerty / 2);
-      if (powerty > 0) {
-        base = applyPerm(base, base);
-      }
-    }
-    return result;
-  }
-
-  const finalPerm = permPower(perm, iterations);
-
-  const resArr = [];
-  for (let i = 0; i < n; i += 1) {
-    resArr[i] = '';
-  }
-  for (let i = 0; i < n; i += 1) {
-    resArr[finalPerm[i]] = str[i];
-  }
-
-  let result = '';
-  for (let i = 0; i < n; i += 1) {
-    result += resArr[i];
-  }
-
-  return result;
+function shuffleChar(/*str, iterations*/) {
+  throw new Error('Not implemented')
 }
 
 /**
